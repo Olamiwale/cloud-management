d57c9d6cec4303f45ec1db99b230e7ca211b8992
unknown <you@example.com>
2023-05-10 23:19:06 +1300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero'
+
+import Support from './components/Support'
+import About from './components/About'
+import Product from './components/Product'
+import Pricing from './components/Pricing'
+import Footer from './components/Footer'
+
+
+function App() {
+
+  return ( 
+
+<div>
+      <Navbar />
+      <Hero />
+      <About />      
+      <Support />
+      <Product />
+      <Pricing />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
+
