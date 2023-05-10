49923e84ed64b20df6e01011de4dcfe77c9c83fd
unknown <you@example.com>
2023-05-11 10:37:02 +1300

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,55 +1,57 @@
-import React from 'react'
+import React from "react";
 import {
-    CloudUploadIcon,
-    DatabaseIcon,
-    PaperAirplaneIcon,
-    ServerIcon,
-} from '@heroicons/react/solid'
+  CloudUploadIcon,
+  DatabaseIcon,
+  PaperAirplaneIcon,
+  ServerIcon,
+} from "@heroicons/react/solid";
 
-import bgImg from '../assets/cyber-bg.png'
+import bgImg from "../assets/cyber-bg.png";
 
-export default function Hero (){
+export default function Hero() {
   return (
-    <div name='home' className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
-        
-        <div className='w-[90%] m-auto grid grid-cols-2 max-sm:flex flex-col'>
-            
-            <div className='flex flex-col justify-center w-full py-8'>
-                <p className='text-2xl'>Unique Sequencing & Production</p>
-                <h1 className='py-3 text-7xl font-bold max-md:text-6xl'>Cloud Management</h1>
-                <p className='text-2xl'>This is our Tech brand.</p>
-                <div className='flex justify-center pt-10'>
-                <button className='border bg-gray-300 w-[300px] p-4 rounded-lg max-md:w-[200px]'>
-                    Get Started</button>
-                </div>
-                
-                
-            </div>
-           
-            <div>
-                <img className='w-full' src={bgImg} alt="/" />
-            </div>
-           
-            <div className='absolute bottom-6 flex flex-col items-center
-             bg-zinc-200 border-slate-300 rounded-xl text-center 
-              w-[700px]
-              shadow-xl max-md:w-[550px] max-sm:w-[300px]'>
-
-
-
-                {/* flex flex-col  md: bottom-[5%]
-            mx-1 md:  bg-zinc-200
-            border border-slate-300 rounded-xl text-center */}
-
-                <p className='text-3xl py-6 font-bold max-md:text-xl'>Data Services</p>
-                <div className='text-3xl flex justify-between p-4 max-md:text-xl max-sm:grid grid-cols-2'>
-                    <p className='flex px-4 py-2 text-slate-500 justify-center items-center'><CloudUploadIcon className='h-6 text-indigo-600' /> App Security</p>
-                    <p className='flex px-4 py-2 text-slate-500 justify-center items-center'><DatabaseIcon className='h-6 text-indigo-600' /> Dashboard Design</p>
-                    <p className='flex px-4 py-2 text-slate-500 justify-center items-center'><ServerIcon className='h-6 text-indigo-600' /> Cloud Data</p>
-                    <p className='flex px-4 py-2 text-slate-500 justify-center items-center'><PaperAirplaneIcon className='h-6 text-indigo-600' /> API</p>
-                </div>
-            </div>
+    <div
+      name="home"
+      className="w-full h-screen bg-zinc-200 flex flex-col justify-between"
+    >
+      <div className="w-[90%] m-auto grid grid-cols-2 max-sm:flex flex-col">
+        <div className="flex flex-col justify-center w-full py-8">
+          <p className="text-2xl">Unique Sequencing & Production</p>
+          <h1 className="py-3 text-7xl font-bold max-md:text-6xl">
+            Cloud Management
+          </h1>
+          <p className="text-2xl">This is our Tech brand.</p>
+          <div className="flex justify-center pt-10">
+            <button className="border bg-gray-300 w-[200px] p-4 rounded-lg max-md:w-[100px]">
+              Get Started
+            </button>
+          </div>
         </div>
+
+        <div>
+          <img className="w-full" src={bgImg} alt="/" />
+        </div>
+
+        <div className="absolute bottom-[6%] max-sm:bottom-0 right-[10%] flex justify-between items-center bg-zinc-300/70 border-slate-300 rounded-xl text-center w-[600px] shadow-xl px-16 py-8 max-md:w-[500px] max-sm:w-[350px] max-sm:px-2">
+          <div className="flex gap-2 max-md:flex flex-col">
+            <CloudUploadIcon className="h-6 text-indigo-600" />
+            <p>Cloud Data</p>
+          </div>
+          <div className="flex gap-2 max-md:flex flex-col">
+            <PaperAirplaneIcon className="h-6 text-indigo-600" />
+            <p>API</p>
+          </div>
+
+          <div className="flex gap-2 max-md:flex flex-col">
+            <DatabaseIcon className="h-6 text-indigo-600" />
+            <p>App security</p>
+          </div>
+          <div className="flex gap-2  max-md:flex flex-col">
+            <ServerIcon className="h-6 text-indigo-600" />
+            <p>Dashboard Design</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -15,11 +15,11 @@ export default function Support (){
       <div className='max-w-[1240px] mx-auto text-white relative'>
           <div className='px-4 py-12'>
               <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Support</h2>
-              <h3 className='text-5xl font-bold py-6 text-center'>Finding the right team</h3>
-              <p className='py-4 text-3xl text-slate-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
+              <h3 className='text-5xl font-bold py-6 text-center max-sm:text-3xl'>Finding the right team</h3>
+              <p className='py-4 text-3xl text-slate-300 max-sm:text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
           </div>
 
-          <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+          <div className='grid grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 max-sm:grid-cols-2 grid-row-3 text-black'>
               <div className='bg-white rounded-xl shadow-2xl'>
                   <div className='p-8'>
                                             <h3 className='font-bold text-2xl my-6'>Sales</h3>
@@ -39,7 +39,8 @@ export default function Support (){
                       <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2' /></p>
                   </div>
               </div>
-              <div className='bg-white rounded-xl shadow-2xl'>
+
+              <div className='bg-white rounded-xl shadow-2xl col-span-2'>
                   <div className='p-8'>
                       
                       <h3 className='font-bold text-2xl my-6'>Media Inquiries</h3>
